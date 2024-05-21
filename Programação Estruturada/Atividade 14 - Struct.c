d1a98d87617bda1dc6a7fd62ae5987564aa211ff
Noemi Soares <142548973+noemisoares@users.noreply.github.com>
2024-05-20 23:10:56 -0300

    Update Atividade 14 - Struct.c

--- Programação Estruturada/Atividade 14 - Struct.c
+++ Programação Estruturada/Atividade 14 - Struct.c
@@ -109,15 +109,6 @@ int main() {
 }
 
 //QUESTÃO 4
-//Crie uma estrutura representando os alunos de um determinado curso. A estrutura deve conter a
-//matrícula do aluno, nome, nota da primeira prova, nota da segunda prova e nota da terceira prova.
-//a. Permita ao usuário entrar com os dados de 5 alunos.
-//b. Encontre o aluno com maior nota da primeira prova.
-//c. Encontre o aluno com maior média geral.
-//d. Encontre o aluno com menor média geral.
-//e. Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para
-//aprovação.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
