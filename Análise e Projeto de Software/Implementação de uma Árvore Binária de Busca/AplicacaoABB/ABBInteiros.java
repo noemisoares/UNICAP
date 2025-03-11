52d4305566db2c1f47e6d48600e7da0c47f816aa
noemisoares <noemisoaresgs@gmail.com>
2025-03-10 22:48:32 -0300

    Create ABBInteiros.java

---
+++ Análise e Projeto de Software/Implementação de uma Árvore Binária de Busca/AplicacaoABB/ABBInteiros.java
@@ -0,0 +1,8 @@
+package AplicacaoABB;
+import ABBGenerica.ABB;
+
+public class ABBInteiros {
+    public static void main(String[] args){
+        ABB<Integer> numeros = new ABB<Integer>();
+    }   
+}
