b7e9ac5f18b247e8d167468136114053b6158ad6
noemisoares <noemisoaresgs@gmail.com>
2025-05-25 16:13:33 -0300

    Create QueueNode.java

---
+++ Árvores e Ordenação de Dados/Projeto - Árvores B/src/Fila/QueueNode.java
@@ -0,0 +1,27 @@
+package Fila;
+
+class QueueNode <T> {
+    private QueueNode<T> prox;
+    private T info;
+
+    QueueNode (T info){
+        this.prox = null;
+        this.info = info;
+    }
+
+    QueueNode<T> getProx(){
+        return this.prox;
+    }
+
+    void setProx(QueueNode<T> prox){
+        this.prox = prox;
+    }
+
+    T getInfo(){
+        return this.info;
+    }
+
+    void setInfo(T info){
+        this.info = info;
+    }
+}
